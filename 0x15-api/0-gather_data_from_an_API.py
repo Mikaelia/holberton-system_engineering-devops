5b1f455399e83046e80100febdc36b0db5302302
Mikaelia <mikaelagurney@gmail.com>
2018-08-27 20:35:19 +0000

    Pep8 fixes

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
+"""
+Queries external API for user todo task information
+"""
 import requests
 import json
 import sys
@@ -6,8 +9,10 @@ import sys
 if __name__ == '__main__':
     user_id = sys.argv[1]
 
-    user = requests.get('https://jsonplaceholder.typicode.com/users', params = {'id': user_id})
-    response = requests.get('https://jsonplaceholder.typicode.com/todos', params={'userId': user_id})
+    user = requests.get('https://jsonplaceholder.typicode.com/users',
+                        params={'id': user_id})
+    response = requests.get('https://jsonplaceholder.typicode.com/todos',
+                            params={'userId': user_id})
 
     tasks = response.json()
     employee = user.json()[0].get('name')
@@ -17,13 +22,12 @@ if __name__ == '__main__':
     task_list = []
 
     for task in tasks:
-        if task.get('completed') == True:
+        if task.get('completed'):
             completed += 1
             task_list.append(task.get('title'))
         total += 1
 
-
-
-    print("Employee {} is done with tasks ({}/{}):".format(employee, completed, total))
+    print("Employee {} is done with tasks ({}/{}):".
+          format(employee, completed, total))
     for task in task_list:
         print("\t {}".format(task))
